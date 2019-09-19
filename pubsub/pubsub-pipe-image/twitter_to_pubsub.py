152af226aa273efd90ab17c57327bd55da8a4305
John <jbencina@users.noreply.github.com>
2019-09-18 21:28:05 -0700

    Added ability to filter by language

--- pubsub/pubsub-pipe-image/twitter_to_pubsub.py
+++ pubsub/pubsub-pipe-image/twitter_to_pubsub.py
@@ -80,4 +80,5 @@ if __name__ == '__main__':
     stream = Stream(auth, listener)
 
     keywords = [s.strip() for s in os.environ['TWKEYWORDS'].split(',')]
-    stream.filter(track=keywords)
+    languages = os.environ['TWKEYWORDS']
+    stream.filter(track=keywords, languages=languages)
